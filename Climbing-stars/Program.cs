2c29ddb131c7a4fb31cb9de076cffcb405667c2f
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 03:05:45 +0200

    Добавьте файлы проекта.

---
+++ Climbing-stars/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int first = 1, second = 2, ways = 0;
+
+        for (int i = 3; i <= n; i++)
+        {
+            ways = first + second;
+            first = second;
+            second = ways;
+        }
+
+        return ways;
+    }
+}
